42c4e1b98ca63f00aa9a300a9e47d6ff364f6ed3
Zack Wagner <Zachary.wagner@wbd.com>
2026-08-26 08:52:09 -0400

    Fix formula: move version before url for interpolation

--- Formula/mac-temp.rb
+++ Formula/mac-temp.rb
@@ -1,8 +1,8 @@
 class MacTemp < Formula
   desc "Lightweight CLI to read CPU and thermal sensor temperatures on Apple Silicon Macs"
   homepage "https://github.com/zackwag/mac-temp"
-  url "https://github.com/zackwag/mac-temp/releases/download/v#{version}/mac-temp"
   version "1.1.0"
+  url "https://github.com/zackwag/mac-temp/releases/download/v#{version}/mac-temp"
   sha256 "31b0a25fb01e1934819bf6061ae8bf7437397354be9494d07908a4b1da551dc0"
   license "MIT"
 
