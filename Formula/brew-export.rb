51bcb27073c806d546a785b38011bedd303ef0ca
Zack Wagner <Zachary.wagner@wbd.com>
2026-09-11 08:54:00 -0400

    Update brew-export to v1.1.1

--- Formula/brew-export.rb
+++ Formula/brew-export.rb
@@ -1,9 +1,9 @@
 class BrewExport < Formula
   desc "Export your Homebrew setup and dotfiles into a portable tarball"
   homepage "https://github.com/zackwag/brew-export"
-  version "1.1.0"
+  version "1.1.1"
   url "https://github.com/zackwag/brew-export/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "d00d4f1c1682dbd0a19af892bdf011e4751ee37048c7352c1cd1d0e293bd67cf"
+  sha256 "a7c135b70463067ee686ddcaaed09f152e3056a2944fa2ace6abe4dc41335af5"
   license "MIT"
 
   depends_on :macos
