d7ee2a9b65dec3061683ceb7756d0a39ed1c77bd
Zack Wagner <zackwag@gmail.com>
2026-09-17 12:26:41 -0400

    chore(dotbuddy): update to 2.2.0 (#5)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Casks/dotbuddy.rb
+++ Casks/dotbuddy.rb
@@ -1,8 +1,8 @@
 cask "dotbuddy" do
-  version "2.1.1"
-  sha256 "c9f30eb8a76606668b26ead7981aa5cdc6163bf0d2568eee037e58375f2ea5dc"
+  version "2.2.0"
+  sha256 "334533a39d2b0355b30885482b4fb20baaff01fedbe5273c4a4e6d8c1bc0e53f"
 
-  url "https://github.com/zackwag/DotBuddy/releases/download/v2.1.1/DotBuddy.zip"
+  url "https://github.com/zackwag/DotBuddy/releases/download/v2.2.0/DotBuddy.zip"
   name "DotBuddy"
   desc "Manage your shell aliases and environment variables"
   homepage "https://github.com/zackwag/DotBuddy"
