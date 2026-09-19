261a6224bdde4bf98c3d5870c277201a60810dc7
Zack Wagner <zackwag@gmail.com>
2026-09-18 20:38:06 -0400

    chore(mac-temp): update to 1.3.1 (#22)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/mac-temp.rb
+++ Formula/mac-temp.rb
@@ -1,7 +1,8 @@
 class MacTemp < Formula
   desc "CLI to read CPU and thermal sensor temperatures on Apple Silicon"
   homepage "https://github.com/zackwag/mac-temp"
-  url "https://github.com/zackwag/mac-temp/releases/download/v1.2.1/mac-temp"
+  url "https://github.com/zackwag/mac-temp/releases/download/v1.3.1/mac-temp"
+  version "1.3.1"
   sha256 "31b0a25fb01e1934819bf6061ae8bf7437397354be9494d07908a4b1da551dc0"
   license "MIT"
 
