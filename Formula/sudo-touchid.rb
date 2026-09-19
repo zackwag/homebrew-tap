e1c2c8035124b340e8fb1a759cde56b1142115e2
Zack Wagner <zackwag@gmail.com>
2026-09-18 20:29:11 -0400

    chore(sudo-touchid): update to 1.1.3 (#20)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -1,7 +1,7 @@
 class SudoTouchid < Formula
   desc "Enable Touch ID for sudo on macOS"
   homepage "https://github.com/zackwag/sudo-touchid"
-  url "https://github.com/zackwag/sudo-touchid/releases/download/v1.1.2/sudo-touchid"
+  url "https://github.com/zackwag/sudo-touchid/releases/download/v1.1.3/sudo-touchid"
   sha256 "b168049c7fb7583b5749619d4591f7e2459a771a8f2c8ed8cba4452a959dd6b5"
   license "MIT"
 
